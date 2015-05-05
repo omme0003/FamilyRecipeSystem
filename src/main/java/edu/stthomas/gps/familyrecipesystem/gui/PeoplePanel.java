c20e90a2dda05b1b66081b861d4f900f543989ff
jommen <juergen.ommen@gmail.com>
2015-05-04 19:54:35 -0500

    Prepared recipe panel for add recipe function

--- src/main/java/edu/stthomas/gps/familyrecipesystem/gui/PeoplePanel.java
+++ src/main/java/edu/stthomas/gps/familyrecipesystem/gui/PeoplePanel.java
@@ -1,36 +1,23 @@
 package edu.stthomas.gps.familyrecipesystem.gui;
 
-import javax.swing.JPanel;
-
-import java.awt.Dimension;
 import java.awt.Color;
-
-import javax.swing.DefaultListModel;
-import javax.swing.JTextField;
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import edu.stthomas.gps.familyrecipesystem.AppSession;
-import edu.stthomas.gps.familyrecipesystem.entity.Family;
-import edu.stthomas.gps.familyrecipesystem.entity.Member;
-import edu.stthomas.gps.familyrecipesystem.entity.Recipe;
-import edu.stthomas.gps.familyrecipesystem.service.FamilyService;
-import edu.stthomas.gps.familyrecipesystem.service.FamilyServiceImpl;
-import edu.stthomas.gps.familyrecipesystem.service.MemberService;
-import edu.stthomas.gps.familyrecipesystem.service.MemberServiceImpl;
-import edu.stthomas.gps.familyrecipesystem.service.RecipeService;
-import edu.stthomas.gps.familyrecipesystem.service.RecipeServiceImpl;
-
-import javax.swing.JList;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import edu.stthomas.gps.familyrecipesystem.AppSession;
+import edu.stthomas.gps.familyrecipesystem.entity.Family;
+import edu.stthomas.gps.familyrecipesystem.entity.Member;
 
 public class PeoplePanel extends JPanel {
 
@@ -42,7 +29,7 @@ public class PeoplePanel extends JPanel {
 	 */
 	public PeoplePanel(ClassPathXmlApplicationContext CTX, final MainWindow parent) {
 		
-		setBackground(Color.WHITE);
+		setBackground(Color.WHITE); 
 		setSize(new Dimension(360, 554));
 		setLayout(null);
 		
